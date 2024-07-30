a7d3a9757f54a3fe91e0753f53246022b2c9de36
Denys Skorov <denys.s@sterl.in>
2024-07-30 19:12:07 +0300

    finish 4 render condition

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import './App.css'
-import Student from "./Student.jsx";
-import PropTypes from "prop-types";
+import UserGreeting from "./UserGreeting.jsx";
 
 function App() {
     return (
         <>
-            <Student name="Ivan" age={30} isStudent={true}/>
-            <Student name="Pavel" age={90} isStudent={false}/>
-            <Student name="Ivan2" age={55} isStudent={true}/>
-            <Student />
+            <UserGreeting  isLoggedIn={true} />
         </>
     );
 }
