3d7b730c204161fb279ba099a92a9bc58fc74d63
Denys Skorov <denys.s@sterl.in>
2024-07-30 19:42:49 +0300

    show fetched posts

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import List from "./List.jsx";
 function App() {
     return (
         <>
-            <List  />
+            <List/>
         </>
     );
 }
--- src/List.jsx
+++ src/List.jsx
@@ -1,8 +1,26 @@
+import {useState} from "react";
+
 function List(props) {
 
+    const [fruits, setFruits] = useState([]);
+
+    fetch('https://jsonplaceholder.typicode.com/posts/?_limit=10')
+        .then((res) => res.json())
+        .then((data) => {
+            setFruits(data)
+        })
+
+    return (
+        <ul>
+            {fruits.map((fruit) => (
+                <li key={fruit.id}>
+                    {fruit.title}
+                </li>
+            ))}
+
+        </ul>
+    )
 }
 
-List.propTypes = {}
-List.defaultProps = {}
 
 export default List
