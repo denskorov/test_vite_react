98d73b9c3a87caa64167ba286d15fc704f603f8c
Denys Skorov <denys.s@sterl.in>
2024-07-30 18:54:40 +0300

    finish 2 topic card

---
+++ src/Card.jsx
@@ -0,0 +1,13 @@
+function Card() {
+    return (
+        <div className="card">
+            <img className="card-img" src="https://maxwellforbes.com/assets/garage/image-test-pages/256x256@3x.png" width={150} alt=""/>
+            <h2 className="card-title">Bro code</h2>
+            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque quisquam, veritatis? Distinctio
+                doloribus eaque inventore iusto laborum nisi nobis nostrum quae quibusdam sequi? Adipisci cupiditate
+                distinctio ducimus iure officiis tempora.</p>
+        </div>
+    )
+}
+
+export default Card
