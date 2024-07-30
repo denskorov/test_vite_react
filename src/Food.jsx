d9e71bc1b93efee00c16b7d44cbd35087eafe0e2
Denys Skorov <denys.s@sterl.in>
2024-07-30 18:43:57 +0300

    finish 1 topic

--- src/Food.jsx
+++ src/Food.jsx
@@ -1,11 +1,14 @@
 function Food() {
+
+
+    const f1 = 'Orange';
+    const f2 = 'Orange1';
+
     return (
         <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
+            <li>Apple</li>
+            <li>{f1}</li>
+            <li>{f2.toUpperCase()}</li>
         </ul>
     )
 }
