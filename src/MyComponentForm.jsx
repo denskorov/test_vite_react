b8e04385f6a727c046515a6cafb3e3c20adfd994
Denys Skorov <denys.s@sterl.in>
2024-07-31 16:18:29 +0300

    added work with state array

--- src/MyComponentForm.jsx
+++ src/MyComponentForm.jsx
@@ -3,52 +3,31 @@ import {useState} from "react";
 function MyComponentForm() {
 
 
-    const [user, setUser] = useState({
-        name: '',
-        age: 20,
-        gender: 'male',
-    });
+    const [foods, setFoods] = useState(['Apple', 'Mango', 'Orange']);
 
-    // const [name, setName] = useState("");
-    // const [age, setAge] = useState(50);
-    // const [gender, setGender] = useState('male');
-    const [comment, setComment] = useState("");
-    const [color, setColor] = useState("#eee");
+    const [newFoodName, setNewFoodName] = useState('');
+    const handleAddFood = () => {
+        setFoods(f => [...f, newFoodName])
+        setNewFoodName('')
+    }
+    const handleRemoveFood = (index) => {
+        setFoods(f => f.filter((_, i) => i !== index))
+    }
 
 
     return <>
+        <h2>List of food</h2>
+        <ul>
+            {foods.map((food, index) => (
+                <li key={index} onClick={() => handleRemoveFood(index)}>
+                    {food}
+                </li>
+            ))}
+        </ul>
         <div>
-            <label htmlFor="">Name</label>
-            <input type="text" value={user.name} onChange={(e) => setUser(u => ({...u, name: e.target.value}))}/>
+            <input type="text" onChange={(e) => setNewFoodName(e.target.value)} value={newFoodName}/>
+            <button onClick={handleAddFood}>Add food</button>
         </div>
-        <div>
-            <label htmlFor="">Age</label>
-            <input type="number" value={user.age} onChange={(e) => setUser(u => ({...u, age: +e.target.value}))}/>
-        </div>
-        <div>
-            <label htmlFor="">Gender</label>
-            <input type="radio" value="male" checked={user.gender === 'male'}
-                   onChange={(e) => setUser(u => ({...u, gender: e.target.value}))}/>
-            <input type="radio" value="female" checked={user.gender === 'female'}
-                   onChange={(e) => setUser(u => ({...u, gender: e.target.value}))}/>
-        </div>
-        <div>
-            <label htmlFor="">Comment</label>
-            <textarea onChange={(e) => setComment(e.target.value)}>{comment}</textarea>
-        </div>
-        <div>
-            <label htmlFor="">Color</label>
-            <input type="color" value={color} onChange={(e) => setColor(e.target.value)}/>
-        </div>
-        <hr/>
-
-        <p><b>Name:</b> {user.name}</p>
-        <p><b>Age:</b> {user.age}</p>
-        <p><b>Gender:</b> {user.gender}</p>
-        <p><b>Comment:</b> {comment}</p>
-        <p><b>Color:</b>
-            <button style={{backgroundColor: color}}>test</button>
-            {color}</p>
     </>
 }
 
