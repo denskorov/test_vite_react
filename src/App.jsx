148050d1601daf460e650a1597637bf01ec433e3
Denys Skorov <denys.s@sterl.in>
2024-07-31 20:50:26 +0300

    useRef with StopWatch.jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import './App.css'
-import {useRef} from "react";
+import StopWatch from "./StopWatch.jsx";
 
 function App() {
-
-
-    const input = useRef(null)
-
     return (
         <>
-            <input ref={input}/>
-            <button onClick={() => input.current.focus()}>Click me!</button>
-
+            <StopWatch/>
+            <StopWatch/>
+            <StopWatch/>
+            <StopWatch/>
         </>
     );
 }
